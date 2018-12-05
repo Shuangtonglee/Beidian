b9c0eaf4b0a14185f8a073c48900a80b58382c4b
Shuangtonglee <shuangtong_lee@163.com>
2018-12-05 23:06:32 +0800

    some test

--- clock.py
+++ clock.py
@@ -14,7 +14,8 @@ sched = BlockingScheduler()
 
 urls = ['https://ibei.cn/1y5uOZ','https://m.beidian.com/detail/detail.html?iid=30409280&shop_id=1480734&utm_source=&offer_code=0']
 
-@sched.scheduled_job('cron', hour='0-23',minute='00')
+@sched.scheduled_job('interval', seconds=60)
+#@sched.scheduled_job('cron', hour='0-23',minute='00')
 def get_data():
     # driver=webdriver.PhantomJS(executable_path='bin/phantomjs',service_args=service_args)
     # sales_number_list = []
@@ -47,6 +48,7 @@ def get_data():
 
 
     for sale in [1,3]:
+        print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),sale)
         ws.append([datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),sale])
     wb.save(filename=filename_path)
 
